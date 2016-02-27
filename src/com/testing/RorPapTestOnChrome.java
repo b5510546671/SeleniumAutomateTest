280a8d6111e2b474226143d6ba6ce4ddb784cab1
Supavit Kongwudhikunakorn <supavit_knot@yahoo.co.th>
2016-02-27 13:43:28 +0700

    added test cases

--- src/com/testing/RorPapTestOnChrome.java
+++ src/com/testing/RorPapTestOnChrome.java
@@ -1,36 +1,20 @@
 package com.testing;
 
-import java.util.List;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class RorPapTestOnChrome {
 	
 	private WebDriver driver;
 	
-	private String firstName;
-	private String lastName;
-	private String emailAddress;
-	private String password1;
-	private String password2;
-	
-	
+	private int counter;
+		
 	public RorPapTestOnChrome(){
+		counter = 0;
 		try {
-			// Setup google chrome driver
-			System.setProperty("webdriver.chrome.driver", "C:\\library\\chromedriver.exe");
-
-			driver = new ChromeDriver();
-			driver.manage().window().maximize();
-			driver.get("http://localhost:8080/#!/");
-			
 			init();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
@@ -39,14 +23,244 @@ public class RorPapTestOnChrome {
 		}
 	}
 	
+	private void createDriver(){
+		// Setup google chrome driver
+		System.setProperty("webdriver.chrome.driver", "C:\\library\\chromedriver.exe");
 
-	public void init() throws InterruptedException {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://localhost:8080/#!/");
 		
-		testSignUpForPasswordMisMatching();
-		testSignUpWithIncorrectDateOfBirth();
-		
+		counter++;
+	}
+	
+	private void destroyDriver(){
 		driver.close();
+	}
+	
 
+	private void init() throws InterruptedException {
+		createDriver();
+		testSignUpForPasswordMisMatching();
+		destroyDriver();
+		createDriver();
+		testSignUpWithIncorrectDateOfBirth();
+		destroyDriver();
+		createDriver();
+		testSignUpWithFutureDateOfBirth();
+		destroyDriver();
+		createDriver();
+		testSignUpWithOutAcceptingPolicy();
+		destroyDriver();
+		createDriver();
+		testSignUpWithInvalidEmailAddress();
+		destroyDriver();
+		createDriver();
+		testSignUpWithCorrectInformation();
+		destroyDriver();
+		createDriver();
+		testLogInWithCreatedAccount();
+		destroyDriver();
+	}
+	
+	/**
+	 * Test for Log in module.
+	 * 		Test for logging in with created account.
+	 * @throws InterruptedException 
+	 */
+	private void testLogInWithCreatedAccount() throws InterruptedException{
+		testSignUpWithCorrectInformation();
+		
+		driver.findElement(By.linkText("Log In")).click();
+				
+		driver.findElement(By.id("emailAddressLogIn")).sendKeys("TesterName".concat(counter+"").concat("@tester.com"));
+		driver.findElement(By.id("password")).sendKeys("123456");
+		
+		driver.findElement(By.id("logInBtn")).click();
+		
+		System.out.println("I'm done with \'Test Log In with created account.\'");
+		
+		Thread.sleep(10000);
+	}
+	
+	/**
+	 * Test for Create User Account module.
+	 * 		Test for signing up with correct information.
+	 * @throws InterruptedException 
+	 */
+	private void testSignUpWithCorrectInformation() throws InterruptedException{
+		String firstName = "TesterName".concat(counter+"");
+		String lastName = "TesterLastName".concat(counter+"");
+		String emailAddress = firstName.concat("@tester.com");
+		String password1 = "123456";
+		String password2 = "123456";
+		
+		driver.findElement(By.linkText("Sign Up")).click();
+		driver.findElement(By.id("firstName")).sendKeys(firstName);
+		driver.findElement(By.id("lastName")).sendKeys(lastName);
+		driver.findElement(By.id("emailAddress")).sendKeys(emailAddress);
+		driver.findElement(By.id("password1")).sendKeys(password1);
+		driver.findElement(By.id("password2")).sendKeys(password2);
+		
+		WebElement dateOfBirth= driver.findElement(By.id("dateOfBirth"));
+		// Set Date to be 10
+		for(int i = 0; i < 10; i++){
+			dateOfBirth.sendKeys(Keys.ARROW_UP);
+		}		
+		dateOfBirth.sendKeys(Keys.TAB);
+		// Set Month to be December
+		for(int i = 0; i < 12; i++){
+			dateOfBirth.sendKeys(Keys.ARROW_UP);
+		}
+		dateOfBirth.sendKeys(Keys.TAB);
+		// Set Year to be 2000
+		for(int i = 0; i < 17; i++){
+			dateOfBirth.sendKeys(Keys.ARROW_DOWN);
+		}
+		
+		if(!driver.findElement(By.id("acceptPolicyLabel")).isSelected()){
+			driver.findElement(By.id("acceptPolicyLabel")).click();
+		}
+		
+		driver.findElement(By.id("signUpBtn")).click();
+				
+		Thread.sleep(3000);
+		
+		System.out.println("I'm done with \'Test Sign Up with correct information.\'");
+	}
+	
+	/**
+	 * Test for Create User Account module.
+	 * 		Test for signing up with invalid email address
+	 * @throws InterruptedException 
+	 */
+	private void testSignUpWithInvalidEmailAddress() throws InterruptedException{
+		String firstName = "น้องน้อตนะจ๊ะ";
+		String lastName = "นามสกุลของน้อต";
+		String emailAddress = "น้องน้อตอีเมลล์จ้า@hotmail.com";
+		String password1 = "123456";
+		String password2 = "123456";
+		
+		driver.findElement(By.linkText("Sign Up")).click();
+		driver.findElement(By.id("firstName")).sendKeys(firstName);
+		driver.findElement(By.id("lastName")).sendKeys(lastName);
+		driver.findElement(By.id("emailAddress")).sendKeys(emailAddress);
+		driver.findElement(By.id("password1")).sendKeys(password1);
+		driver.findElement(By.id("password2")).sendKeys(password2);
+		
+		WebElement dateOfBirth= driver.findElement(By.id("dateOfBirth"));
+		// Set Date to be 10
+		for(int i = 0; i < 10; i++){
+			dateOfBirth.sendKeys(Keys.ARROW_UP);
+		}		
+		dateOfBirth.sendKeys(Keys.TAB);
+		// Set Month to be December
+		for(int i = 0; i < 12; i++){
+			dateOfBirth.sendKeys(Keys.ARROW_UP);
+		}
+		dateOfBirth.sendKeys(Keys.TAB);
+		// Set Year to be 2000
+		for(int i = 0; i < 17; i++){
+			dateOfBirth.sendKeys(Keys.ARROW_DOWN);
+		}
+		
+		if(!driver.findElement(By.id("acceptPolicyLabel")).isSelected()){
+			driver.findElement(By.id("acceptPolicyLabel")).click();
+		}
+		
+		driver.findElement(By.id("signUpBtn")).click();
+				
+		Thread.sleep(3000);
+		
+		System.out.println("I'm done with \'Test Sign Up with invalid email address.\'");
+	}
+	
+	/**
+	 * Test for Create User Account module.
+	 * 		Test for signing up without accepting policy.
+	 * @throws InterruptedException 
+	 */
+	private void testSignUpWithOutAcceptingPolicy() throws InterruptedException{
+		String firstName = "TesterName".concat(counter+"");
+		String lastName = "TesterLastName".concat(counter+"");
+		String emailAddress = firstName.concat("@tester.com");
+		String password1 = "123456";
+		String password2 = "123456";
+		
+		driver.findElement(By.linkText("Sign Up")).click();
+		driver.findElement(By.id("firstName")).sendKeys(firstName);
+		driver.findElement(By.id("lastName")).sendKeys(lastName);
+		driver.findElement(By.id("emailAddress")).sendKeys(emailAddress);
+		driver.findElement(By.id("password1")).sendKeys(password1);
+		driver.findElement(By.id("password2")).sendKeys(password2);
+		
+		WebElement dateOfBirth= driver.findElement(By.id("dateOfBirth"));
+		// Set Date to be 10
+		for(int i = 0; i < 10; i++){
+			dateOfBirth.sendKeys(Keys.ARROW_UP);
+		}		
+		dateOfBirth.sendKeys(Keys.TAB);
+		// Set Month to be December
+		for(int i = 0; i < 12; i++){
+			dateOfBirth.sendKeys(Keys.ARROW_UP);
+		}
+		dateOfBirth.sendKeys(Keys.TAB);
+		// Set Year to be 2000
+		for(int i = 0; i < 17; i++){
+			dateOfBirth.sendKeys(Keys.ARROW_DOWN);
+		}
+		
+		driver.findElement(By.id("signUpBtn")).click();
+				
+		Thread.sleep(3000);
+		
+		System.out.println("I'm done with \'Test Sign Up without accepting policy.\'");
+	}
+	
+	/**
+	 * Test for Create User Account module.
+	 * 		Test for signing up when the date of birth is in the future.
+	 * @throws InterruptedException 
+	 */
+	private void testSignUpWithFutureDateOfBirth() throws InterruptedException{
+		String firstName = "TesterName".concat(counter+"");
+		String lastName = "TesterLastName".concat(counter+"");
+		String emailAddress = firstName.concat("@tester.com");
+		String password1 = "123456";
+		String password2 = "123456";
+		
+		driver.findElement(By.linkText("Sign Up")).click();
+		driver.findElement(By.id("firstName")).sendKeys(firstName);
+		driver.findElement(By.id("lastName")).sendKeys(lastName);
+		driver.findElement(By.id("emailAddress")).sendKeys(emailAddress);
+		driver.findElement(By.id("password1")).sendKeys(password1);
+		driver.findElement(By.id("password2")).sendKeys(password2);
+		
+		WebElement dateOfBirth= driver.findElement(By.id("dateOfBirth"));
+		// Set Date to be 30
+		for(int i = 0; i < 30; i++){
+			dateOfBirth.sendKeys(Keys.ARROW_UP);
+		}		
+		dateOfBirth.sendKeys(Keys.TAB);
+		// Set Month to be April
+		for(int i = 0; i < 4; i++){
+			dateOfBirth.sendKeys(Keys.ARROW_UP);
+		}
+		dateOfBirth.sendKeys(Keys.TAB);
+		// Set Year to be 2030
+		for(int i = 0; i < 15; i++){
+			dateOfBirth.sendKeys(Keys.ARROW_UP);
+		}
+				
+		if(!driver.findElement(By.id("acceptPolicyLabel")).isSelected()){
+			driver.findElement(By.id("acceptPolicyLabel")).click();
+		}
+		
+		driver.findElement(By.id("signUpBtn")).click();
+				
+		Thread.sleep(3000);
+		
+		System.out.println("I'm done with \'Test Sign Up When Date of Birth is the future date.\'");
 	}
 	
 	/**
@@ -55,12 +269,18 @@ public class RorPapTestOnChrome {
 	 * @throws InterruptedException 
 	 */
 	private void testSignUpWithIncorrectDateOfBirth() throws InterruptedException {
+		String firstName = "TesterName".concat(counter+"");
+		String lastName = "TesterLastName".concat(counter+"");
+		String emailAddress = firstName.concat("@tester.com");
+		String password1 = "123456";
+		String password2 = "123456";
+		
 		driver.findElement(By.linkText("Sign Up")).click();
-		driver.findElement(By.id("firstName")).sendKeys("TesterName1");
-		driver.findElement(By.id("lastName")).sendKeys("TesterLastName1");
-		driver.findElement(By.id("emailAddress")).sendKeys("testerX2@tester.com");
-		driver.findElement(By.id("password1")).sendKeys("123456");
-		driver.findElement(By.id("password2")).sendKeys("123456");
+		driver.findElement(By.id("firstName")).sendKeys(firstName);
+		driver.findElement(By.id("lastName")).sendKeys(lastName);
+		driver.findElement(By.id("emailAddress")).sendKeys(emailAddress);
+		driver.findElement(By.id("password1")).sendKeys(password1);
+		driver.findElement(By.id("password2")).sendKeys(password2);
 		
 		WebElement dateOfBirth= driver.findElement(By.id("dateOfBirth"));
 		// Set Date to be 31
@@ -93,13 +313,19 @@ public class RorPapTestOnChrome {
 	 * 		Test for signing up when password mismatch.
 	 * @throws InterruptedException 
 	 */
-	public void testSignUpForPasswordMisMatching() throws InterruptedException{
+	private void testSignUpForPasswordMisMatching() throws InterruptedException{
+		String firstName = "TesterName".concat(counter+"");
+		String lastName = "TesterLastName".concat(counter+"");
+		String emailAddress = firstName.concat("@tester.com");
+		String password1 = "123456";
+		String password2 = "654321";
+		
 		driver.findElement(By.linkText("Sign Up")).click();
-		driver.findElement(By.id("firstName")).sendKeys("TesterName");
-		driver.findElement(By.id("lastName")).sendKeys("TesterLastName");
-		driver.findElement(By.id("emailAddress")).sendKeys("testerX1@tester.com");
-		driver.findElement(By.id("password1")).sendKeys("123456");
-		driver.findElement(By.id("password2")).sendKeys("654321");
+		driver.findElement(By.id("firstName")).sendKeys(firstName);
+		driver.findElement(By.id("lastName")).sendKeys(lastName);
+		driver.findElement(By.id("emailAddress")).sendKeys(emailAddress);
+		driver.findElement(By.id("password1")).sendKeys(password1);
+		driver.findElement(By.id("password2")).sendKeys(password2);
 		
 		WebElement dateOfBirth= driver.findElement(By.id("dateOfBirth"));
 		dateOfBirth.sendKeys(Keys.ARROW_UP);
